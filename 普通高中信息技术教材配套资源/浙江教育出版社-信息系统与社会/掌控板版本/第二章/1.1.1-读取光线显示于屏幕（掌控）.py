c3877f12e97a8625021485ab9bc217db97a2950e
xiezuoru <8334180@qq.com>
2022-05-08 17:02:21 +0800

    xiezuoru
    更新

--- 普通高中信息技术教材配套资源/浙江教育出版社-信息系统与社会/掌控板版本/第二章/1.1.1-读取光线显示于屏幕（掌控）.py
+++
@@ -1,7 +0,0 @@
-from mpython import *
-import time
-while True:
-    display.fill(0)
-    display.DispChar("光线值：%d"%light.read(), 0, 0)
-    display.show()
-    time.sleep(0.2)
--- 普通高中信息技术教材配套资源/浙江教育出版社-信息系统与社会/掌控板版本/第二章/1.1.2-读取光线于串口输出（掌控）.py
+++
@@ -1,5 +0,0 @@
-from mpython import *
-import time
-while True:
-    print(light.read())
-    time.sleep(0.2)
